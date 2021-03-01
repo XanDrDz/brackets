88467787efab9b53120c498f3f703c20d46c29dd
XanDrDz <64421185+XanDrDz@users.noreply.github.com>
2021-03-01 17:30:17 +0500

    fix: try solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const brack = "(){}[]"; // перечень скобок
+    let arr = []; // новый массив
+    for (let i=0; i<str.length; i++) {
+        let symb = str[i]; //i - ый символ
+        let ind = brack.indexOf(symb); // поиск символа в скобках
+        if (ind === -1) {
+            continue
+        };
+        if (ind % 2 === 0) {
+            arr.push(ind + 1) 
+        } else {
+            if (arr.pop() !== ind) {
+                return false;
+            }
+        }
 }
+return arr.length === 0;
+} 
